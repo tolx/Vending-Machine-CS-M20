bd0244970ec219d6866a87879c419be862fbf5d9
AshnakAGQ <justinpianist1@gmail.com>
2016-12-10 15:36:55 -0800

    Removed Legacy Code

--- FinalProject/FinalProject/VendingMachine.h
+++ FinalProject/FinalProject/VendingMachine.h
@@ -6,64 +6,20 @@ Kelton Malhotra
 Justin Kephart
 */
 
-#include "Products.h"
+#include "Slot.h"
 #include "Graph.h"
 
 static const double COIN_MAX = 1.50;
 
-/*
-enum States
-{
-	IDLE,
-	DISPLAY_PRICE,
-	CARD_ACCEPTED,
-	CASH_INSERTED,
-	REFUND,
-	DISPENCE_PRODUCT
-
-};
-*/
-
-
 class VendingMachine
 {
 private:
-
-	/*
-	Prod List (key = prodCode)
-	Prodct code , prod name and prices
-
-	D4 , "soda ", 70
-	D5 , Water, 80
-	D6 , Juice, 90
-	*/
-
 	Graph<string, string> statesMachine;
-
 	map <string, class prod> prodList;
-
-	/*
-	typedef void (VendingMachine::*DoAction)(void);
-	*/
 
 	void BuildProdList();
 	void BuildStateMachine();
 	void GoToIdleState();
-
-	// Callbacks called by state machine
-
-	/*
-	void DispenseProduct();
-	void DisplayProdPrice();
-	void CardSwiped();
-	void CashInserted();
-	void OrderCancelled();
-	
-
-	void CancelCardTransaction();
-	void Refund(double coins);
-
-	*/
 
 	bool GoToNextState(string transition);
 
@@ -72,8 +28,6 @@ private:
 	bool  paidByCreditCard;
 	double total_coins;
 	string currentState;
-	
-
 
 public:
 	VendingMachine();
@@ -83,5 +37,4 @@ public:
 	void swipeCard(string cardType);
 	void cancelOrder();
 	void coinReturn();
-
 };
