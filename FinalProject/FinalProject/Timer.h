fbe18047b587cf9739bd17d39922da9c47393dfd
RileyWallace <school.wallace@gmail.com>
2016-12-11 01:26:17 -0800

    Small Timer changes, updated To Do list
    added //Finished? to a few items I believe someone else completed. To
    avoid me removing them too early, anyone who can confirm should remove
    the items with // from the list

--- FinalProject/FinalProject/Timer.h
+++ FinalProject/FinalProject/Timer.h
@@ -19,12 +19,16 @@ class Timer
     double secsToTimeout;
 
 public:
+    //Default constructor sets time threshold to 2 minutes (120 seconds)
     Timer() : startTime(clock()), secsToTimeout(120) {}
     Timer(double minTimeout) : startTime(clock()), secsToTimeout(minTimeout) {}
 
+
     //Returns time elapsed in seconds. (ex. 2.783)
     double timeElapsed() { return ((clock() - startTime) / CLOCKS_PER_SEC); }
-    bool timeout() { return (secsToTimeout <= timeElapsed()); }
+    //returns true if timeElapsed exceeds the set time for timing out
+    bool timedOut() { return (secsToTimeout <= timeElapsed()); }
+    //resets timer
     void updateTime() { startTime = clock(); }
 };
 
